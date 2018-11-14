d739734e7b8e131485ca5f864d0a00f6c89f980e
Philipp Vujic <newtimestube@gmail.com>
2018-11-14 10:44:46 +0100

    Header Add

---
+++ header.php
@@ -0,0 +1,29 @@
+<header class="head">
+    <div class="container-fluid">
+        <nav class="navbar navbar-expand-md">
+            <img src="img/logo.png" alt="" class="header-logo">
+            <ul id="nav-ul" class="navbar-nav">
+                <li class="nav-item ml-3"><a href="#" class="nav-link"><i
+                                class="fas fa-home"></i> HOUSES</a>
+                </li>
+                <li class="nav-item ml-3"><a href="#" class="nav-link"><i
+                                class="fas fa-list-alt"></i>
+                        RESERVATIONS</a></li>
+                <li class="nav-item ml-3"><a href="#" class="nav-link"><i
+                                class="fas fa-question-circle"></i>
+                        SUPPORT</a></li>
+            </ul>
+            <div id="header-right">
+                <ul id="nav-ul" class="navbar-nav">
+                    <li class="nav-item"><a href="#" class="nav-link"><i
+                                    class="far fa-comments"></i></a></li>
+                    <li class="nav-item mr-2"><a href="#" class="nav-link"><i
+                                    class="far fa-bell"></i></a></li>
+                    <li class="nav-item ml-5"><a href="#"></li>
+                    <img src="img/tempprofile.jpg" alt=""
+                         class="header-profile-img rounded-circle">
+                    </a>
+                </ul>
+            </div>
+    </div>
+</header>
