6ea0a7d9a85def7544ec2aa115cbbfe9871d7d85
Philipp Vujic <newtimestube@gmail.com>
2018-11-16 13:00:34 +0100

    DAO

--- MVC/models/DAO/DAO.php
+++ MVC/models/DAO/DAO.php
@@ -6,19 +6,66 @@
  * Time: 12:20
  */
 
-class DAO extends PDO {
+abstract class DAO extends PDO {
+    private $table;
+    private $db;
 
     private static $instance = null;
 
-    /**
-     * DAO constructor.
-     */
-    private function __construct() {
+
+    private function __construct($table) {
+        $this->table=(string) $table;
+
         $config = Config::singleton();
         $dsn = 'mysql:host=' . $config->get('dbhost') . ';dbname=' . $config->get('dbname');
         $user = $config->get('dbuser');
         $pass = $config->get('dbpass');
         parent::__construct($dsn, $user, $pass);
+    }
+
+    public function db(){
+        return $this->db;
+    }
+
+    public function getAll(){
+        $statement = "SELECT * FROM $this->table ORDER BY id DESC";
+        $res = db()->prepare($statement);
+        $res->execute();
+
+        //Devolvemos el resultset en forma de array de objetos
+        $rows = $res->fetchAll(PDO::FETCH_ASSOC);
+
+        return $rows;
+    }
+
+    public function getById($id){
+        $query=$this->db->query("SELECT * FROM $this->table WHERE id=$id");
+
+        if($row = $query->fetch_object()) {
+            $resultSet=$row;
+        }
+
+        return $resultSet;
+    }
+
+    public function getBy($column,$value){
+        $query=$this->db->query("SELECT * FROM $this->table WHERE $column='$value'");
+
+        while($row = $query->fetch_object()) {
+            $resultSet[]=$row;
+        }
+
+        return $resultSet;
+    }
+
+    public function deleteById($id){
+        $query=$this->db->query("DELETE FROM $this->table WHERE id=$id");
+        return $query;
+    }
+
+    public function deleteBy($column,$value){
+        $query=$this->db->query("DELETE FROM $this->table WHERE $column='$value'");
+        return $query;
     }
 
 
