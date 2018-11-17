e2ade8ee09a01a1d6d8db5b253497bdc031659f8
Yuami <j_fornes_1999@hotmail.com>
2018-11-17 20:06:43 +0100

    Listados

---
+++ public/js/selects/selectVivienda-Tarifa.js
@@ -0,0 +1,13 @@
+let list = $('#list-group-vivienda-tarifa');
+let response = fetch("info/vivienda-tarifa.php");
+let json = response.then(response => response.json());
+let listItems = json.then(json => json.map(item => {
+    let s = `Vivienda: ${item.vivienda} Tarifa: ${item.tarifa}`;
+    return toListItem(s);
+}));
+
+listItems.then(items => list.append(items));
+
+function toListItem(item) {
+    return `<li class="list-group-item">${item}</li>`
+}
