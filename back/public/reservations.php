370c5e835e9b226d88c02c5095cfd62c530bf0ae
MagoBeodo <trolconlove@gmail.com>
2018-11-23 13:44:08 +0100

    intentando dataTables de reservations

--- back/public/reservations.php
+++ back/public/reservations.php
@@ -13,17 +13,49 @@
 </head>
 
 <body>
-    <?php include_once("header.php") ?>
-    <section>
-        <h1>Reservations</h1>
-        <div id="cardscasa" class="container">
-            <h2>Cards Reserva </h2>
-        </div>
-    </section>
+<?php include_once("header.php") ?>
+<!--  <section>
+     <h1>Reservations</h1>
+     <div id="cardscasa" class="container">
+         <h2>Cards Reserva </h2>
+     </div>
+ </section> -->
+<table id="reservaList" class="table">
+    <!-- <tbody >
+  -   <tr>
+         <td>
+             <i class="fas fa-home">Casa</i>
+         </td>
+         <td>
+             <h6>Nom:</h6>
+             <p class="font-weight-bold">Piolin</p>
+         </td>
+         <td>
+             <h6>Usuari:</h6>
+             <p class="font-weight-bold">Ruben Dario</p>
+         </td>
+         <td>
+             <h6>Estat:</h6>
+             <p class="font-weight-bold text-orange">Pendent</p>
+         </td>
+         <td>
+             <h6>Preu:</h6>
+             <p class="font-weight-bold text-danger">500</p>
+         </td>
+         <td>
+             <h6>Data Reserva:</h6>
+             <p class="font-weight-bold">12/12/2015<br>15/12/2015</p>
+         </td>
+         <td>
+             <p id="missatge" class="font-weight-bold">Missatge: <span> N </span><span><i class="far fa-envelope">Missatge</i></span></p>
+         </td>
+     </tr>
+    </tbody> -->
+</table>
 
-
-    <?php include_once("footer.php") ?>
-    <script src="js/selects/selectReservaById.js"></script>
+<?php include_once("footer.php") ?>
+<script src="js/selects/selectReservaById.js"></script>
+<script src="js/selects/selectReservasListByIdVivienda.js"></script>
 </body>
 
 </html>
