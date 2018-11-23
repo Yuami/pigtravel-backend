2adbbd78fd7730ad8e0f5ce4d38449486674c2a9
JoanFornesRayo <j_fornes_1999@hotmail.com>
2018-11-23 13:59:59 +0100

    Cockies - Auth - Session - DAO

---
+++ back/app/config/Cookie.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: j_for
+ * Date: 23/11/2018
+ * Time: 13:47
+ */
+
+class Cookie {
+
+    public static function get($key) {
+        if (self::isSet($key)){
+            return $_COOKIE[$key];
+        }
+        return false;
+    }
+
+    public static function isSet($key) {
+        return isset($_COOKIE[$key]);
+    }
+
+    public static function set($key, $value, $timeInDays) {
+        $timeToDays = $timeInDays * 24 * 60 * 60 * 60;
+        setcookie($key, $value, $timeToDays);
+    }
+
+    public static function delete($key) {
+        if (self::isSet($key)){
+            $_COOKIE[$key] = null;
+        }
+    }
+}
