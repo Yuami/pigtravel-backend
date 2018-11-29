27136a9b17e2daca5d4754ff081095d0260a6761
ruben <rrodriguez13671@alumnes.iesmanacor.cat>
2018-11-29 09:09:18 +0100

    actualizada Reservations # Conflicts: #	back/public/info/conn.php

--- back/public/info/selectReservasList.php
+++ back/public/info/selectReservasList.php
@@ -7,13 +7,13 @@
  */
 header("Content-Type: application/json");
 require_once("conn.php");
-$stmt = $conn->prepare("select v.nombre nomVivienda, p.nombre nomPersona, ehi.nombre nomEstat,fechaReserva  , precio
+$stmt = $conn->prepare("select v.nombre nomVivienda, p.nombre nomPersona, ehi.nombre nomEstat,fechaReserva  , precio preu
 from vivienda v
        inner join reserva r on v.id = r.idVivienda
        inner join cliente c on idPersona = idCliente
        inner join persona p on p.id = idPersona
        inner join estado e on e.id = r.id
-       inner join estado_has_idioma ehi on e.id = idEstado where r.idVivienda=1");
+       inner join estado_has_idioma ehi on e.id = idEstado");
 $stmt->execute();
 $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 $json = json_encode($result);
--- back/public/reservations.php
+++ back/public/reservations.php
@@ -21,42 +21,32 @@
          <h2>Cards Reserva </h2>
      </div>
  </section> -->
-<table id="reservaList" class="table">
-    <!-- <tbody >
-  -   <tr>
-         <td>
-             <i class="fas fa-home">Casa</i>
-         </td>
-         <td>
-             <h6>Nom:</h6>
-             <p class="font-weight-bold">Piolin</p>
-         </td>
-         <td>
-             <h6>Usuari:</h6>
-             <p class="font-weight-bold">Ruben Dario</p>
-         </td>
-         <td>
-             <h6>Estat:</h6>
-             <p class="font-weight-bold text-orange">Pendent</p>
-         </td>
-         <td>
-             <h6>Preu:</h6>
-             <p class="font-weight-bold text-danger">500</p>
-         </td>
-         <td>
-             <h6>Data Reserva:</h6>
-             <p class="font-weight-bold">12/12/2015<br>15/12/2015</p>
-         </td>
-         <td>
-             <p id="missatge" class="font-weight-bold">Missatge: <span> N </span><span><i class="far fa-envelope">Missatge</i></span></p>
-         </td>
-     </tr>
-    </tbody> -->
-</table>
+<div class="container col-10">
+    <div class="container">
+        <h1>Gestio Reservas</h1>
+        <h3><u><a href="login.php">Pagina Principal > </a><a href="houselist.php">Cases</a></u></h3>
+        <div class="dropdown">
+            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton"
+                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Estats
+            </button>
+            <div id="dropEst" class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            </div>
+        </div>
+        
+        <div class="align-self-center">
+            <table class="table table-striped">
+                <tbody id="reservaList">
 
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
 <?php include_once("footer.php") ?>
-<script src="js/selects/selectReservaById.js"></script>
-<script src="js/selects/selectReservasListByIdVivienda.js"></script>
+<script src="js/selects/selectReservasList.js"></script>
+<script src="js/selects/selectEstado.js"></script>
+
 </body>
 
 </html>
