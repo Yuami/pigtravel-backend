a7fb20410bed392bb6172ad54839d36f2d3faa2c
Joan Fornés <j_fornes_1999@hotmail.com>
2018-12-02 17:38:58 +0100

    Vivienda DAO

--- back/app/models/DAO/ViviendaDAO.php
+++ back/app/models/DAO/ViviendaDAO.php
@@ -11,28 +11,52 @@ class ViviendaDAO extends DAO{
         // TODO: Implement insert() method.
     }
 
+    /**
+     * @param $tipo
+     * @return array
+     */
     public static function getByTipoVivienda($tipo)
     {
         return parent::getBy("idTipoVvienda", $tipo);
     }
 
+    /**
+     * @param $bool
+     * @return array
+     */
     public static function getByAlquilerAutomatico($bool)
     {
         return parent::getBy("alquilerAutomatico", $bool);
     }
 
+    /**
+     * @param $id
+     * @return array
+     */
     public static function getByIDCiudad($id)
     {
         return parent::getBy("idCiudad", $id);
     }
 
+    /**
+     * @param $bool
+     * @return array
+     */
     public static function getByDestacada($bool)
     {
         return parent::getBy("destacada", $bool);
     }
 
+    /**
+     * @param $capacidad
+     * @return array
+     */
     public static function getByCapacidad($capacidad)
     {
-        $statement = DB::conn()->prepare("SELECT ");
+        $statement = DB::conn()->prepare("SELECT * FROM vivienda where capacidad >= :capacidad");
+        $statement->bindValue(":capacidad", $capacidad, PDO::PARAM_INT);
+        $statement->execute();
+
+        return $statement->fetchAll(PDO::FETCH_CLASS, self::$table);
     }
 }
