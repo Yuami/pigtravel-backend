b649c54887d63af3375e28cf9a4a9d6608e70f32
PepToniBibiloni <peptonibibiloni@gmail.com>
2018-12-04 12:00:13 +0100

    messages

--- back/public/js/selects/selectMensajes.js
+++ back/public/js/selects/selectMensajes.js
@@ -1,10 +1,11 @@
 $(function () {
-    function loadCardsMensajes() {
+    var botonLeido=false;
+    var viviendaID;
+    function loadCardsMensajes(botonLeido,viviendaId) {
         var xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function () {
             if (this.readyState == 4 && this.status == 200) {
                 var mensajeX = JSON.parse(this.responseText);
-                var i = 0;
                 var ccard="";
                 for (x in mensajeX) {
 
@@ -20,30 +21,48 @@ $(function () {
                     var fechaEnviado = mensajeX[x].fechaEnviado;
                     var leido = mensajeX[x].leido;
 
-                    if(leido==1) {
-                        ccard =
-                            "<div class='card-body missatge'>" +
-                            "<div class='row'>" +
-                            "<div class='col-md-4'>" + nombre + "</div>" +
-                            "<div class='col-md-6'>" + mensaje + "</div>" +
-                            "<div class='col-md-2'>" + fechaEnviado + "</div>" +
-                            "</div>" +
-                            "<div class='row'>" +
-                            "<div class='col-md-3'>" + nombreCasa + "</div>" +
-                            "</div>" +
-                            "</div>";
-                    }else{
-                        ccard =
-                        "<div class='card-body missatge'>" +
-                        "<div class='row'>" +
-                        "<div class='col-md-4'><b>" + nombre + "</b></div>" +
-                        "<div class='col-md-6'><b>" + mensaje + "</b></div>" +
-                        "<div class='col-md-2'><b>" + fechaEnviado + "</b></div>" +
-                        "</div>" +
-                        "<div class='row'>" +
-                        "<div class='col-md-3'><b>" + nombreCasa + "</b></div>" +
-                        "</div>" +
-                        "</div>";
+                      if(botonLeido==true) {
+                          if(leido==1) {
+                              ccard =
+                                  "<div class='card-body missatge'>" +
+                                  "<div class='row'>" +
+                                  "<div class='col-md-4'>" + nombre + "</div>" +
+                                  "<div class='col-md-6'>" + mensaje + "</div>" +
+                                  "<div class='col-md-2'>" + fechaEnviado + "</div>" +
+                                  "</div>" +
+                                  "<div class='row'>" +
+                                  "<div class='col-md-5'>" + nombreCasa + "</div>" +
+                                  "</div>" +
+                                  "</div>";
+                          }else {
+                              ccard = "";
+                          }
+                      }else{
+                          if(leido==1) {
+                              ccard =
+                                  "<div class='card-body missatge'>" +
+                                  "<div class='row'>" +
+                                  "<div class='col-md-4'>" + nombre + "</div>" +
+                                  "<div class='col-md-6'>" + mensaje + "</div>" +
+                                  "<div class='col-md-2'>" + fechaEnviado + "</div>" +
+                                  "</div>" +
+                                  "<div class='row'>" +
+                                  "<div class='col-md-5'>" + nombreCasa + "</div>" +
+                                  "</div>" +
+                                  "</div>";
+                          }else {
+                          ccard =
+                              "<div class='card-body missatge'>" +
+                              "<div class='row'>" +
+                              "<div class='col-md-4'><b>" + nombre + "</b></div>" +
+                              "<div class='col-md-6'><b>" + mensaje + "</b></div>" +
+                              "<div class='col-md-2'><b>" + fechaEnviado + "</b></div>" +
+                              "</div>" +
+                              "<div class='row'>" +
+                              "<div class='col-md-5'><b>" + nombreCasa + "</b></div>" +
+                              "</div>" +
+                              "</div>";
+                      }
                     }
                     rowDIV.append(ccard);
 
@@ -51,65 +70,54 @@ $(function () {
 
             }
         };
-        xhttp.open("GET", "info/selectMensajes.php", true);
-        xhttp.send();
+         if(viviendaId!=null) {
+             xhttp.open("GET", "info/selectMensajes.php?idVivienda=" + viviendaId, true);
+             xhttp.send();
+         }else{
+             xhttp.open("GET", "info/selectMensajes.php", true);
+             xhttp.send();
+         }
     }
 
     loadCardsMensajes();
 
-
     $("#botonLeido").click( function() {
+            $(".cardMessages").empty();
+            if(botonLeido==false){
+                botonLeido=true;
+            }else{
+                botonLeido=false;
+            }
+            loadCardsMensajes(botonLeido,viviendaID);
+        }
+    );
+    function loadViviendasDropdown() {
         var xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function () {
             if (this.readyState == 4 && this.status == 200) {
-                var mensajeX = JSON.parse(this.responseText);
-                var i = 0;
-                var ccard="";
-                $(".cardMessages").empty();
-                for (x in mensajeX) {
-
-                    if (i % 2 == 0) {
-                        var rowDIV = $("<div />", {
-                            class: "card cardMessages",
-                        });
-                        $("#cardsmensajes").append(rowDIV);
-                    }
-                    i++;
-
-                    var nombre = mensajeX[x].nombreSender;
-                    var nombreCasa = mensajeX[x].nombreCasa;
-                    var mensaje = mensajeX[x].mensaje;
-                    var fechaEnviado = mensajeX[x].fechaEnviado;
-                    var leido = mensajeX[x].leido;
-
-                    if(leido==0) {
-                        ccard =
-                            "<div class='card-body missatge'>" +
-                            "<div class='row'>" +
-                            "<div class='col-md-4'>" + nombre + "</div>" +
-                            "<div class='col-md-6'>" + mensaje + "</div>" +
-                            "<div class='col-md-2'>" + fechaEnviado + "</div>" +
-                            "</div>" +
-                            "<div class='row'>" +
-                            "<div class='col-md-3'>" + nombreCasa + "</div>" +
-                            "</div>" +
-                            "</div>";
-                    }else{
-                        ccard ="";
-                    }
-                    rowDIV.append(ccard);
-
+                var viviendas = JSON.parse(this.responseText);
+                for (x in viviendas) {
+                    var nomVivenda = viviendas[x].vivienda;
+                    var viviendaId=viviendas[x].id;
+                    dropd= "<option value="+viviendaId+">"+nomVivenda+"</option>";
+                    $("#listaViviendas").append(dropd);
                 }
-
             }
         };
-        xhttp.open("GET", "info/selectMensajes.php", true);
+        xhttp.open("GET", "info/selectViviendaLista.php", true);
         xhttp.send();
+    }
 
+    loadViviendasDropdown();
+    document.getElementById("listaViviendas").onchange = function() {
+        $(".cardMessages").empty();
+        viviendaID=this.value;
+        botonLeido=false;
+        if(viviendaID==-1) {
+            loadCardsMensajes(botonLeido);
+        }else{
+            loadCardsMensajes(botonLeido,viviendaID);
+        }
 
-            }
-
-
-    );
-
+    };
 });
