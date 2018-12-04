050a5c3efe8e1bfb89d012f7312abb1d6091e7b0
ruben <rrodriguez13671@alumnes.iesmanacor.cat>
2018-12-04 19:19:04 +0100

    update reservations y houses # Conflicts: #	back/public/info/conn.php #   back/public/css/style.css

--- back/public/js/selects/selectViviendaLista.js
+++ back/public/js/selects/selectViviendaLista.js
@@ -1,34 +1,49 @@
 $(function () {
-    function loadViviendasLista() {
-        var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function () {
-            if (this.readyState == 4 && this.status == 200) {
-                var reserves = JSON.parse(this.responseText);
+        function loadViviendasLista() {
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function () {
+                if (this.readyState == 4 && this.status == 200) {
+                    var reserves = JSON.parse(this.responseText);
 
-                for (reserva in reserves) {
-                    var idReserva = reserves[reserva].id;
-                    var nomVivenda = reserves[reserva].vivienda;
-                    var tipoVivienda = reserves[reserva].tipo;
-                    var ciudadVivienda = reserves[reserva].ciudad;
+                    for (reserva in reserves) {
+                        var idReserva = reserves[reserva].id;
+                        var nomVivenda = reserves[reserva].vivienda;
+                        var tipoVivienda = reserves[reserva].tipo;
+                        var ciudadVivienda = reserves[reserva].ciudad;
 
-                    var url = "house.php?id=" + idReserva;
-                    appendToLlista("<ul>");
-                    appendToLlista($("<a/>", {href:url,class:'list-group-item list-group-item-action active', text:idReserva}));
-                    appendToLlista($("<a/>", {href:url,class:'list-group-item list-group-item-action', text:nomVivenda}));
-                    appendToLlista($("<a/>", {href:url,class:'list-group-item list-group-item-action', text:tipoVivienda}));
-                    appendToLlista($("<a/>", {href:url,class:'list-group-item list-group-item-action mb-3', text:ciudadVivienda}));
-                    appendToLlista("</ul>");
+                        var url = "house.php?id=" + idReserva;
+                        var newA = $("<a/>", {
+                            href: url,
+                            class: 'card col-8 align-self-center',
+                            style: 'color: inherit; margin-left: 17%; text-decoration:none;'
+                        });
+                        var newCBody = $("<div/>", {class: 'card-body row'});
+                        var newCImg = $("<div/>", {class: 'card-image col-4'});
+                        var newI = $("<img/>", {src: "img/casas/house" + idReserva + ".jpg", style: 'max-width: 60%'});
+                        var newCCont = $("<div/>", {class: 'row col-8 '});
+                        var newPN = $("<p/>", {text: nomVivenda, class: 'col font-weight-bold list-group-item py-5 text-center'});
+                        var newPC = $("<p/>", {text: ciudadVivienda, class: 'col font-weight-bold list-group-item'});
+                        var newPP = $("<p/>", {text: "Peticiones Reserva", class: 'col font-weight-bold list-group-item'});
+                        var newPM = $("<p/>", {text: "Mensajes", class: 'col font-weight-bold list-group-item'});
+                        newCCont.append(newPN, newPC, newPP, newPM);
+                        newCImg.append(newI);
+                        newCBody.append(newCImg, newCCont);
+                        newA.append(newCBody);
+                        appendToLlista(newA);
 
+                    }
                 }
             }
-        };
-        xhttp.open("GET", "info/selectViviendaLista.php", true);
-        xhttp.send();
-    }
+            ;
+            xhttp.open("GET", "info/selectViviendaLista.php", true);
+            xhttp.send();
+        }
 
-    function appendToLlista(item){
-        $("#llistaReserves").append(item);
-    }
+        function appendToLlista(item) {
+            $("#cardCasa").append(item);
+        }
 
-    loadViviendasLista();
-});
+        loadViviendasLista();
+    }
+)
+;
--- back/public/reservations.php
+++ back/public/reservations.php
@@ -16,9 +16,6 @@
     <link rel="stylesheet" href="https://cdn.datatables.net/select/1.2.7/css/select.bootstrap4.min.css"/>
 
     <!-- js -->
-
-    <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
-
     <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
 
     <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
@@ -30,50 +27,44 @@
 
 <body>
 <?php include_once("header.php") ?>
-<a href="reserva.php"><h1>RESERVA</h1></a>
 <div class="container col-10">
-    <div class="row">
-        <div class="col">
-            <h3>Gestio Reservas</h3>
-            <ul class="bg-transparent breadcrumb">
-                <li class="breadcrumb-item">
-                    <a href="#">Pagina Principal</a>
-                </li>
-                <li class="breadcrumb-item active">
-                    <a href="#">Gestio Reservas</a>
-                </li>
-            </ul>
-            <div class="row">
-
-                <div class="form-group col-3 form-check-inline">
-                    <h6 class="col-4">Filtrar por</h6>
-                    <select id="filterEst" class="form-control">
-
-                    </select>
-                </div>
-                <div class="form-group col-3 form-check-inline">
-                    <h6 class="col-4">Filtrar por</h6>
-                    <select id="filterViv" class="form-control">
-
-                    </select>
-                </div>
-                <div class=" col-2">
-                    <button id="clean" class="btn btn-danger"><i class="fas fa-times-circle"></i></button>
-                </div>
-            </div>
-            <table id="taula" class="table table-striped table-bordered">
-                <thead>
-                <tr>
-                    <th>Casa</th>
-                    <th>Cliente</th>
-                    <th>Estado</th>
-                    <th>Fecha</th>
-                    <th>Precio</th>
-                </tr>
-                </thead>
-            </table>
+    <div class="row breadcrumb-row">
+        <div class="col-md-10 offset-md-1">
+            <h1>Gestio Reservas</h1>
+            <ol class="bg-transparent pt-0 breadcrumb">
+                <li class="breadcrumb-item"><a href="#">Home</a></li>
+                <li class="breadcrumb-item active" aria-current="page">Gestio Reservas</li>
+            </ol>
         </div>
     </div>
+    <div class="row">
+        <div class="form-group col-3 form-check-inline">
+            <h6 class="col-4">Filtrar por</h6>
+            <select id="filterEst" class="form-control">
+
+            </select>
+        </div>
+        <div class="form-group col-3 form-check-inline">
+            <h6 class="col-4">Filtrar por</h6>
+            <select id="filterViv" class="form-control">
+
+            </select>
+        </div>
+        <div class=" col-2">
+            <button id="clean" class="btn btn-danger"><i class="fas fa-times-circle"></i></button>
+        </div>
+    </div>
+    <table id="taula" class="table table-striped table-bordered">
+        <thead>
+        <tr>
+            <th>Casa</th>
+            <th>Cliente</th>
+            <th>Estado</th>
+            <th>Fecha</th>
+            <th>Precio</th>
+        </tr>
+        </thead>
+    </table>
 </div>
 <?php include_once("footer.php") ?>
 <script src="js/selects/selectReservasList.js"></script>
