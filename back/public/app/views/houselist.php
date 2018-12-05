02f978c9ee2008ea3e51216b7ff6bd3b7922d4c0
ruben <rrodriguez13671@alumnes.iesmanacor.cat>
2018-12-05 08:35:46 +0100

    register datepicker # Conflicts: #	back/public/info/conn.php

--- back/public/app/views/houselist.php
+++ back/public/app/views/houselist.php
@@ -12,55 +12,19 @@
 
 <body>
 <?php include_once("header.php") ?>
-<section>
-    <h1><a href="house.phplic/routes.php">House</a></h1>
-    <h1>Houses</h1>
-
-    <div class="row">
-        <div class="col">
-            <table id="listaCasas" class="display table table-striped table-bordered" style="width: 100%;">
-                <thead>
-                <tr>
-                    <th>House</th>
-                    <th>Tipo Vivienda</th>
-                    <th>Capacity</th>
-                    <th>Street</th>
-                    <th>Ciudad</th>
-                    <th>Check In</th>
-                    <th>Check Out</th>
-                    <th>Alquiler Automatico</th>
-                    <th>Square Meters</th>
-                </tr>
-                </thead>
-            </table>
+<section class="container-fluid">
+    <div class="row breadcrumb-row">
+        <div class="col-md-10 offset-md-1">
+            <h1>Gestio Cases</h1>
+            <ol class="bg-transparent pt-0 breadcrumb">
+                <li class="breadcrumb-item"><a href="#">Home</a></li>
+                <li class="breadcrumb-item active" aria-current="page">Gestio Cases</li>
+            </ol>
         </div>
     </div>
-
-    <div class="dropdown">
-        <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">
-            Tipo Viviendas
-        </button>
-        <div class="dropdown-menu" id="dropdownidTipoVivienda">
-        </div>
-    </div>
-
-    <div class="input-group mb-3">
-        <select class="custom-select" id="selectVendedor">
-            <option selected>Vendedor...</option>
-        </select>
-        <select class="custom-select" id="selectCasas">
-            <option selected>Tipo de Casa...</option>
-        </select>
-    </div>
-
-    <table class="table table-striped" id="listadoViviendas">
-
-    </table>
-
-    <div id="llistaReserves">
+    <div class="row" id="cardCasa">
 
     </div>
-
 
 </section>
 
--- back/public/app/views/reservations.php
+++ back/public/app/views/reservations.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <?php include_once("libraries.php") ?>
+    <?php require_once ROOT . "libraries.php" ?>
     <!-- css -->
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.1/css/bootstrap.css"/>
