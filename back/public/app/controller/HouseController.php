e00e7a48bab3e8b56e92e1ae5f996005bdef2b37
ruben <rrodriguez13671@alumnes.iesmanacor.cat>
2018-12-17 13:23:42 +0100

    register sin acabar # Conflicts: #	back/public/info/conn.php

--- back/public/app/controller/HouseController.php
+++ back/public/app/controller/HouseController.php
@@ -4,7 +4,7 @@ class HouseController extends Controller
 {
     public function index()
     {
-        include_once VIEW . "houseList.php";
+        include_once VIEW . "houselist.php";
     }
 
     public function show()
