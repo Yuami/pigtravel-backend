77154bff58467a3998734edcb74dbf590fc73a2d
PepToniBibiloni <peptonibibiloni@gmail.com>
2018-12-18 11:09:55 +0100

    messages

--- back/public/js/selects/selectMensajes.js
+++ back/public/js/selects/selectMensajes.js
@@ -1,13 +1,16 @@
 $(document).ready(function() {
     $( "#mobilebutton" ).click(function() {
+         if(this.value==0) {
+             $('[id=0]').hide();
+             this.value=1;
+         }else{
+             $('[id=0]').show();
+             this.value=0;
+         }
         $(this)
             .find('[id="icon"]')
             .toggleClass('fa-eye-slash')
             .toggleClass('fa-eye');
-
-        $("#cardsmensajes").load("#cardsmensajes < *");
-        });
-    $( "#listaViviendas" ).onchange(function() {
-
     });
+
 });
