c1b37a95ad6280a445d2b9a33c0957221395e052
JoanFornesRayo <j_fornes_1999@hotmail.com>
2019-01-02 14:12:53 +0100

    changes reservation done

--- back/App/View/reservation.php
+++ back/App/View/reservation.php
@@ -27,10 +27,10 @@ $cliente = $reserva->getCliente();
         <div class="row">
             <div class="col-md-8">
                 <div class="card p-3 bg-light">
-                    <a href="/houses/<?php echo $vivienda->getId() ?>">
-                        <div class="card-show">
-                            <h6>CASA</h6>
-                            <div class="card">
+                    <div class="">
+                        <h6>CASA</h6>
+                        <a href="/houses/<?php echo $vivienda->getId() ?>">
+                            <div class="card card-show">
                                 <div class="card-body">
                                     <div class="card-text">
                                         <div class="row">
@@ -40,72 +40,73 @@ $cliente = $reserva->getCliente();
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                        </div>
-                    </a>
+                        </a>
+                    </div>
+                </div>
 
-                    <div class="mt-3">
-                        <h6>CLIENTE</h6>
-                        <div class="card">
+                <div class="mt-3">
+                    <h6>CLIENTE</h6>
+                    <a href="/profile/<?php echo $cliente->getID() ?>">
+                        <div class="card card-show">
                             <div class="card-body">
                                 <div class="card-text">
                                     <div class="row">
                                         <img src="/<?php echo File::getProfileImageById($reserva->getIdCliente()) ?>"
                                              alt="" class="vivienda-img rounded-circle">
                                         <h5 class="ml-2 mt-3"><?php echo $cliente->getNombre() ?></h5>
-                                        <a href="/profile/<?php echo $cliente->getID() ?>"
-                                           class="btn btn-primary ml-auto my-3 px-4 mr-2">VER</a>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    </a>
+                </div>
 
-                    <div class="mt-3">
-                        <h6>INGRESOS</h6>
-                        <div class="card">
-                            <div class="card-body">
-                                <div class="card-text">
-                                    <div class="row">
-                                        <p class="ml-2 mt-3"><b>TOTAL:</b> $<?php echo $reserva->getPrecio() ?></p>
-                                    </div>
+                <div class="mt-3">
+                    <h6>INGRESOS</h6>
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="card-text">
+                                <div class="row">
+                                    <p class="ml-2 mt-3"><b>TOTAL:</b> $<?php echo $reserva->getPrecio() ?></p>
                                 </div>
                             </div>
                         </div>
                     </div>
-
-                    <div class="mt-3">
-                        <button class="btn btn-primary" onclick="showCalendar('#calendars')">Calendario</button>
-                    </div>
                 </div>
-            </div>
-            <div class="col-md-4">
-                <a href="/messages/<?php echo $vivienda->getID(); ?>" class="btn btn-primary mt-3 col-12">CHAT
-                    CON <?php echo strtoupper($cliente->getNombre()); ?></a>
-                <a href="#nothing" class="btn btn-primary mt-3 col-12">EDITAR/OFERTA</a>
-                <a href="#nothing" class="btn btn-primary mt-3 col-12">ACEPTAR</a>
-                <a href="#nothing" class="btn btn-danger mt-3 col-12">CANCELAR</a>
+
+                <div class="mt-3">
+                    <button class="btn btn-primary" onclick="showCalendar('#calendars')">Calendario</button>
+                </div>
             </div>
         </div>
-        <!-- CALENDARS -->
-        <div id="calendars" style="display: none">
-            <div class="row pb-3">
-                <div class="col-md-6">
-                    <div id="mainCalendar" data-f_inicio="<?php echo $reserva->getCheckIn(); ?>" data-f_fin="<?php echo $reserva->getCheckOut(); ?>"></div>
-                </div>
-                <div class="col-md-6">
-                    <div id="nextCalendar"></div>
-                </div>
-            </div>
-
-            <button type="button" class="fc-prev-button btn btn-primary" id="prevMonth">
-                <span class="fa fa-chevron-left"></span> PREV
-            </button>
-
-            <button type="button" class="fc-prev-button btn btn-primary" id="nextMonth">
-                NEXT <span class="fa fa-chevron-right"></span>
-            </button>
+        <div class="col-md-4">
+            <a href="/messages/<?php echo $vivienda->getID(); ?>" class="btn btn-primary mt-3 col-12">CHAT
+                CON <?php echo strtoupper($cliente->getNombre()); ?></a>
+            <a href="#nothing" class="btn btn-primary mt-3 col-12">EDITAR/OFERTA</a>
+            <a href="#nothing" class="btn btn-primary mt-3 col-12">ACEPTAR</a>
+            <a href="#nothing" class="btn btn-danger mt-3 col-12">CANCELAR</a>
         </div>
+    </div>
+    <!-- CALENDARS -->
+    <div id="calendars" style="display: none">
+        <div class="row pb-3">
+            <div class="col-md-6">
+                <div id="mainCalendar" data-f_inicio="<?php echo $reserva->getCheckIn(); ?>"
+                     data-f_fin="<?php echo $reserva->getCheckOut(); ?>"></div>
+            </div>
+            <div class="col-md-6">
+                <div id="nextCalendar"></div>
+            </div>
+        </div>
+
+        <button type="button" class="fc-prev-button btn btn-primary" id="prevMonth">
+            <span class="fa fa-chevron-left"></span> PREV
+        </button>
+
+        <button type="button" class="fc-prev-button btn btn-primary" id="nextMonth">
+            NEXT <span class="fa fa-chevron-right"></span>
+        </button>
+    </div>
     </div>
 </section>
 
