1e7a078294a330647e787de75c650f60c00b75f0
philippvujic <newtimestube@gmail.com>
2019-01-07 22:25:52 +0100

    house

--- back/App/View/house.php
+++ back/App/View/house.php
@@ -7,11 +7,142 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <?php require_once ROOT . "libraries.php" ?>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"></script>
+    <link rel="stylesheet" href="/css/leaflet.css">
+    <script src="/js/leaflet.js"></script>
     <title>House</title>
 </head>
 <body class="bg-color-background">
 <?php include_once("header.php") ?>
-<section class="container">
+<nav class="navbar navbar-expand-lg navbar-dark" id="scrollspy">
+    <ul class="nav nav-pills mr-auto ml-auto">
+        <li class="nav-item text-center col-6 col-sm-3"><a class="nav-link" href="#section1">House</a></li>
+        <li class="nav-item text-center col-6 col-sm-3"><a class="nav-link" href="#section2">Rates</a></li>
+        <li class="nav-item text-center col-6 col-sm-3"><a class="nav-link" href="#section3">Policies</a></li>
+        <li class="nav-item text-center col-6 col-sm-3"><a class="nav-link" href="#">Show</a></li>
+    </ul>
+</nav>
+
+<section class="container"  id="mainHouseSection" >
+    <div class="row breadcrumb-row">
+        <div class="col-md-10 offset-md-1">
+            <h1>House</h1>
+            <ol class="bg-transparent pt-0 breadcrumb">
+                <li class="breadcrumb-item"><a href="/">Home</a></li>
+                <li class="breadcrumb-item"><a href="/houses">House Management</a></li>
+                <li class="breadcrumb-item active" aria-current="page">House</li>
+            </ol>
+        </div>
+    </div>
+    <div class="container-fluid">
+        <form method="POST" action="/houses/<?= $vInfo->getId(); ?>">
+            <input type="hidden" name="_method" value="PUT">
+
+            <div class="row">
+                <div class="col-md-6 ">
+                    <h2 class="text-center">Information</h2>
+                    <div class="row">
+                        <label class="sr-only" for="firstNameForm">House Name</label>
+                        <div class="input-group mb-2 mr-sm-2">
+                            <h6 class="text-md-right my-auto mr-sm-3 col-md-2">House Name</h6>
+                            <div class="input-group-prepend">
+                                <div class="input-group-text"><span class="fas fa-home text-danger"></span>
+                                </div>
+                            </div>
+                            <input type="text" class="form-control col-md-8" id="houseName" name="houseName"
+                                   style="width: 80px;" value="<?= $vInfo->getNombre(); ?>">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <label class="sr-only" for="peopleAmount">Capacity</label>
+                        <div class="input-group mb-2 mr-sm-2">
+                            <h6 class="text-md-right my-auto mr-sm-3 col-md-2">Capacity</h6>
+                            <div class="input-group-prepend">
+                                <div class="input-group-text"><span class="fas fa-user text-danger"></span>
+                                </div>
+                            </div>
+                            <input type="number" class="form-control col-md-8" id="peopleAmount" name="peopleAmount"
+                                   style="width: 80px;" value="<?= $vInfo->getCapacidad(); ?>">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <label class="sr-only" for="street">Street</label>
+                        <div class="input-group mb-2 mr-sm-2">
+                            <h6 class="text-md-right my-auto mr-sm-3 col-md-2">Street</h6>
+                            <div class="input-group-prepend">
+                                <div class="input-group-text"><span class="fas fa-road text-danger"></span>
+                                </div>
+                            </div>
+                            <input type="text" class="form-control col-md-8" id="street" name="street"
+                                   style="width: 80px;" value="<?= $vInfo->getCalle(); ?>">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <label class="sr-only" for="city">City</label>
+                        <div class="input-group mb-2 mr-sm-2">
+                            <h6 class="text-md-right my-auto mr-sm-3 col-md-2">City</h6>
+                            <div class="input-group-prepend">
+                                <div class="input-group-text"><span class="fas fa-city text-danger"></span>
+                                </div>
+                            </div>
+                            <select id="city" class="form-control col-md-8" name="city">
+                            </select>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <label class="sr-only" for="squaremeters">Square meters</label>
+                        <div class="input-group mb-2 mr-sm-2">
+                            <h6 class="text-md-right my-auto mr-sm-3 col-md-2">Square meters</h6>
+                            <div class="input-group-prepend">
+                                <div class="input-group-text"><span class="fas fa-arrows-alt-h text-danger"></span>
+                                </div>
+                            </div>
+                            <input type="text" class="form-control col-md-8" id="squaremeters" name="squaremeters"
+                                   style="width: 80px;" value="<?= $vInfo->getMetrosCuadrados(); ?>">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <label class="sr-only" for="checkIn">Check In</label>
+                        <div class="input-group mb-2 mr-sm-2">
+                            <h6 class="text-md-right my-auto mr-sm-3 col-md-2">Check In</h6>
+                            <div class="input-group-prepend">
+                                <div class="input-group-text"><span class="fas fa-lock-open text-danger"></span>
+                                </div>
+                            </div>
+                            <input type="time" class="form-control col-md-8" id="checkIn" name="checkIn"
+                                   style="width: 80px;" value="<?= $vInfo->getHoraEntrada(); ?>">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <label class="sr-only" for="checkOut">Check Out</label>
+                        <div class="input-group mb-2 mr-sm-2">
+                            <h6 class="text-md-right my-auto mr-sm-3 col-md-2">Check Out</h6>
+                            <div class="input-group-prepend">
+                                <div class="input-group-text"><span class="fas fa-lock text-danger"></span>
+                                </div>
+                            </div>
+                            <input type="time" class="form-control col-md-8" id="checkOut" name="checkOut"
+                                   style="width: 80px;" value="<?= $vInfo->getHoraSalida(); ?>">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <label for="description" class="sr-only">Description</label>
+                        <h6 class="text-md-right my-auto mr-sm-3 col-md-2">Description</h6>
+
+                        <textarea id="description" class="form-control mb-1 col-md-8" rows="4" cols="50"
+                                  name="description"><?= $vInfo->getDescripcion(); ?></textarea>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <h2 class="text-center">Map</h2>
+                    <div id="houseMap" style="width:auto;height:300px;"></div>
+                </div>
+            </div>
+            <input class="btn btn-block btn-primary mt-3" type="submit"/>
+
+        </form>
+    </div>
+
+
     <div class="card">
         <div class="card-title row justify-content-center">
             <h1><?php echo $vInfo->getNombre() ?></h1>
@@ -171,6 +302,14 @@
         }
     }
 </script>
-
+<script src="/js/custom/house.js"></script>
+<script src="/js/custom/loadLocalidades.js"></script>
+<script>
+    $(function () {
+        let idCiudadVivienda = <?= $vInfo->getIdCiudad(); ?>;
+        loadLocalidades(idCiudadVivienda);
+        mapLoad(<?php echo $vInfo->getCoordX() . "," . $vInfo->getCoordY(); ?>);
+    });
+</script>
 <?php include_once("footer.php") ?>
 <script src="/js/selects/selectTarifa-Rebaja.js"
