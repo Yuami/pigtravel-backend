f6668627e687b895f0c00559c5235cee4a8f6727
peptoni <peptonibibiloni@gmail.com>
2019-01-10 13:51:53 +0100

    support

---
+++ back/App/Controller/SupportController.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jansu
+ * Date: 10/01/19
+ * Time: 12:45
+ */
+
+namespace Controller;
+use Config\Session;
+use Model\DAO\PersonaDAO;
+
+class SupportController extends Controller
+{
+
+    public function show($id) {
+
+    }
+
+    public function index() {
+        $id = Session::get('userID');
+        $persona = PersonaDAO::getById($id);
+        include_once VIEW . 'support.php';
+    }
+
+
+    public function create() {
+        // TODO: Implement create() method.
+    }
+
+    public function store() {
+        $to = 'admin@admin.com';
+        $subject = 'the subject';
+        $message = $_POST["comment"];
+        $headers = 'From: '.$_POST["mail"].'' . "\r\n" .
+            'Reply-To: '.$to.'' . "\r\n" .
+            'X-Mailer: PHP/' . phpversion();
+
+        mail($to, $subject, $message, $headers);
+        header('Location: ' . DOMAIN. ('/'));
+    }
+
+    public function edit($id) {
+        // TODO: Implement edit() method.
+    }
+
+    public function update($id) {
+        // TODO: Implement update() method.
+    }
+
+    public function destroy() {
+        // TODO: Implement destroy() method.
+    }
+}
