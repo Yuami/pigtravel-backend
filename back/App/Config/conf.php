6c25e6161da2bd1ac49582318ca8eb2cdef09819
JoanFornesRayo <j_fornes_1999@hotmail.com>
2019-01-11 23:45:19 +0100

    House default image Composer minor changes

--- back/App/Config/conf.php
+++ back/App/Config/conf.php
@@ -5,19 +5,17 @@ use Config\Config;
 // TODO: Errors only on dev
 Config::errorsOn();
 
-
-
 $config = Config::singleton();
 
-//local
+//server
 //$config->set('dbhost', 'sql142.main-hosting.eu');
 //$config->set('dbname', 'u333704226_pigtr');
 //$config->set('dbuser', 'u333704226_pigtr');
 //$config->set('dbpass', 'ifc21B17*');
 
 
-//server
+//local
 $config->set('dbhost', 'localhost');
 $config->set('dbname', 'travel2');
 $config->set('dbuser', 'root');
-$config->set('dbpass', '');
+$config->set('dbpass', 'tfff01');
