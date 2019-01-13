0e24ac91e3748de36dc317e208f9d8f9f1f86d12
philippvujic <newtimestube@gmail.com>
2019-01-13 21:46:33 +0100

    Encryption and Countries

--- back/App/Config/conf.php
+++ back/App/Config/conf.php
@@ -8,14 +8,14 @@ Config::errorsOn();
 $config = Config::singleton();
 
 //server
-//$config->set('dbhost', 'sql142.main-hosting.eu');
-//$config->set('dbname', 'u333704226_pigtr');
-//$config->set('dbuser', 'u333704226_pigtr');
-//$config->set('dbpass', 'ifc21B17*');
-
-
-//local
 $config->set('dbhost', 'localhost');
 $config->set('dbname', 'travel2');
 $config->set('dbuser', 'phil');
 $config->set('dbpass', 'ifc21B17*');
+
+
+//local
+//$config->set('dbhost', 'localhost');
+//$config->set('dbname', 'travel2');
+//$config->set('dbuser', 'root');
+//$config->set('dbpass', '');
