198c13039f238448bf0ceca29cbef6ba96c9c110
ruben <rrodriguez13671@alumnes.iesmanacor.cat>
2019-01-15 09:57:07 +0100

    creando TarifaController tarifa.php

---
+++ back/App/Controller/TarifasController.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ruben
+ * Date: 15/01/2019
+ * Time: 9:52
+ */
+
+namespace Controller;
+
+
+class TarifasController extends Controller
+{
+
+    public function index()
+    {
+        include_once VIEW . 'tarifas.php';
+    }
+
+    public function create()
+    {
+        // TODO: Implement create() method.
+    }
+
+    public function store()
+    {
+        // TODO: Implement store() method.
+    }
+
+    public function show($id)
+    {
+        // TODO: Implement show() method.
+    }
+
+    public function edit($id)
+    {
+        // TODO: Implement edit() method.
+    }
+
+    public function update($id)
+    {
+        // TODO: Implement update() method.
+    }
+
+    public function destroy()
+    {
+        // TODO: Implement destroy() method.
+    }
+}
