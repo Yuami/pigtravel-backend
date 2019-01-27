0c40bfaf8b9c7cfa00ebb49bc0f02eacc18ad3f0
Joan Fornés <j_fornes_1999@hotmail.com>
2019-01-27 17:53:31 +0100

    Error display added

--- back/App/Model/DAO/DB.php
+++ back/App/Model/DAO/DB.php
@@ -1,24 +1,35 @@
 <?php
-namespace Model\DAO;
-use Config\Config;
-use PDO;
 
-class DB {
+namespace Model\DAO;
+
+use Config\Config;
+use Config\Err;
+use PDO;
+use Routing\Router;
+
+class DB
+{
     private static $conn;
 
-    private function __construct() {
+    private function __construct()
+    {
         $config = Config::singleton();
         $dsn = 'mysql:host=' . $config->get('dbhost') . ';dbname=' . $config->get('dbname');
         $user = $config->get('dbuser');
         $pass = $config->get('dbpass');
-        self::$conn = new PDO($dsn, $user, $pass);
-        self::$conn->exec("set names utf8");
+        try {
+            self::$conn = new PDO($dsn, $user, $pass);
+            self::$conn->exec("set names utf8");
+        } catch (\Exception $e){
+            Err::display('Problema al conectar con la base de datos! Intetalo más tarde!','500');
+        }
     }
 
     /**
      * @return PDO
      */
-    public static function conn(): PDO {
+    public static function conn(): PDO
+    {
         if (!isset($conn))
             new self;
         return self::$conn;
