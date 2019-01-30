c5c1e9d9de5da3e4e29ce2f42e5c3db4eeb4fe92
JoanFornesRayo <j_fornes_1999@hotmail.com>
2019-01-30 10:22:45 +0100

    Auth general, mostrar error con Auth::setError(mensaje)

--- back/App/Controller/MessagesController.php
+++ back/App/Controller/MessagesController.php
@@ -1,52 +1,77 @@
 <?php
+
 namespace Controller;
 
 use Config\Session;
+use Handler\Auth;
 use Model\DAO\MensajesDAO;
 use Model\DAO\PersonaDAO;
+use Model\DAO\ReservaDAO;
 use Model\DAO\ViviendaDAO;
+use Routing\Router;
 
-class MessagesController extends Controller {
+class MessagesController extends Controller
+{
 
-    public function show($id) {
-        $mensajes = MensajesDAO::getByidReserva($id);
-        $viviendas= ViviendaDAO::getBy('idVendedor',Session::get('userID'));
-        require_once VIEW . 'messages.php';
+    public function show($id)
+    {
+        $reserva = ReservaDAO::getById($id);
+        if (isset($reserva) && Auth::verifyVendedor($reserva->getVendedor()->getId())) {
+            $mensajes = MensajesDAO::getByidReserva($id);
+            $viviendas = ViviendaDAO::getBy('idVendedor', Session::get('userID'));
+            $recievers = array();
+            if (isset($mensajes))
+                foreach ($mensajes as $mensaje) {
+                    $recievers[$mensaje->getIdSender()] = PersonaDAO::getBy("id", $mensaje->getIdSender());
+                }
+            require_once VIEW . 'messages.php';
+        } else {
+            Auth::setError("You do not have access to that messages");
+            Router::redirect("messages");
+        }
     }
 
-    public function index() {
-        $id= Session::get('userID');
-        $mensajes = MensajesDAO::getBy("idReciever",$id);
+    public function index()
+    {
+        $id = Session::get('userID');
+        $mensajes = MensajesDAO::getBy("idReciever", $id);
         $mensajesChat = MensajesDAO::getChat($id);
-        $recievers=array();
+        $recievers = array();
         foreach ($mensajes as $mensaje) {
             $recievers[$mensaje->getIdSender()] = PersonaDAO::getBy("id", $mensaje->getIdSender());
         }
-        $viviendas= ViviendaDAO::getBy('idVendedor',$id);
+        $viviendas = ViviendaDAO::getBy('idVendedor', $id);
         require_once VIEW . 'messages.php';
     }
-    public function create() {
+
+    public function create()
+    {
         // TODO: Implement create() method.
     }
 
-    public function store() {
+    public
+    function store()
+    {
         MensajesDAO::leido($_POST['idMensaje']);
         MensajesDAO::insert([
             "idReciever" => $_POST['idReciever'],
             "mensaje" => $_POST['mensajeRespuesta'],
             "idVivienda" => $_POST['idVivienda']]);
-        header("Location: " . DOMAIN. "/messages");
+        header("Location: " . DOMAIN . "/messages");
     }
 
-    public function edit($id) {
+    public function edit($id)
+    {
         // TODO: Implement edit() method.
     }
 
-    public function update($id) {
+    public function update($id)
+    {
         // TODO: Implement update() method.
     }
 
-    public function destroy() {
+    public function destroy()
+    {
         // TODO: Implement destroy() method.
     }
 
