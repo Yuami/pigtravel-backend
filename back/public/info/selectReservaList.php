211ef6f65a6336f105f5c29bbd91c384979ce3d1
ruben <rrodriguez13671@alumnes.iesmanacor.cat>
2019-02-01 13:35:21 +0100

    no consigo hacer los selects

--- back/public/info/selectReservaList.php
+++ back/public/info/selectReservaList.php
@@ -5,24 +5,18 @@
  * Date: 21/11/18
  * Time: 17:31
  */
-
-use Config\Session;
-use Model\DAO\DB;
-
 header("Content-Type: application/json");
 require_once("conn.php");
-$idV = (int)Session::get('userID');
 if (isset($_GET['vivienda'])) {
     $sql = "SELECT v.nombre nom
 FROM vivienda v
        inner join reserva r on v.id = r.idVivienda
        inner join reserva_has_estado rhe on r.id = rhe.idReserva
        inner join estado_has_idioma ehi
-                  on ehi.idEstado = rhe.idEstado and ehi.nombre='" . $_GET['estado'] . "' where idVendedor = :idV";
-    $stm = DB::conn()->prepare($sql);
-    $stm->bindValue(":idV", $idV);
+                  on ehi.idEstado = rhe.idEstado and ehi.nombre='" . $_GET['estado'] . "' where idVendedor = 9";
+    $stmt = $conn->prepare($sql);
 } else {
-    $sql = "select v.nombre   nomVivienda,
+    $stmt = $conn->prepare("select v.nombre   nomVivienda,
        p.nombre   nomPersona,
        ehi.nombre nomEstat,
        e.id       idEstat,
@@ -34,12 +28,10 @@ from vivienda v
        inner join reserva_has_estado rhe on r.id = rhe.idReserva
        inner join persona p on r.idCliente = p.id
        inner join estado e on rhe.idEstado = e.id
-       inner join estado_has_idioma ehi on e.id = ehi.idEstado where idVendedor= :idV";
-    $stm = DB::conn()->prepare($sql);
-    $stm->bindValue(":idV", $idV);
+       inner join estado_has_idioma ehi on e.id = ehi.idEstado where idVendedor=9");
 }
-$stm->execute();
-$result = $stm->fetchAll(PDO::FETCH_ASSOC);
+$stmt->execute();
+$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 $json = json_encode($result);
 echo $json;
 ?>
