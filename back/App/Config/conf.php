a9de6ad077d4699d9c23681b53b0c35cf2d6dd0f
JoanFornesRayo <j_fornes_1999@hotmail.com>
2019-02-04 11:21:25 +0100

    conf

--- back/App/Config/conf.php
+++ back/App/Config/conf.php
@@ -3,19 +3,19 @@
 use Config\Config;
 
 // TODO: Errors only on dev
-Config::errorsOn();
+//Config::errorsOn();
 
 $config = Config::singleton();
 
 //server
-//$config->set('dbhost', 'localhost');
-//$config->set('dbname', 'travel');
-//$config->set('dbuser', 'jfornes');
-//$config->set('dbpass', 'tfff01');
+$config->set('dbhost', 'localhost');
+$config->set('dbname', 'travel');
+$config->set('dbuser', 'jfornes');
+$config->set('dbpass', 'tfff01');
 
 
 //local
- $config->set('dbhost', 'localhost');
- $config->set('dbname', 'travel');
- $config->set('dbuser', 'root');
- $config->set('dbpass', '');
+// $config->set('dbhost', 'localhost');
+// $config->set('dbname', 'travel');
+// $config->set('dbuser', 'root');
+// $config->set('dbpass', '');
