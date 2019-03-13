505a4a6310a769efacbab5ee3a3828526f18e720
ruben <rrodriguez13671@alumnes.iesmanacor.cat>
2019-03-13 09:15:05 +0100

    unchecked primer tarifa

--- back/App/Config/conf.php
+++ back/App/Config/conf.php
@@ -8,14 +8,14 @@ Config::errorsOn();
 $config = Config::singleton();
 
 //server
-//$config->set('dbhost', 'localhost');
-//$config->set('dbname', 'travel');
-//$config->set('dbuser', 'jfornes');
-//$config->set('dbpass', 'tfff01');
+$config->set('dbhost', 'localhost');
+$config->set('dbname', 'travel');
+$config->set('dbuser', 'jfornes');
+$config->set('dbpass', 'tfff01');
 
 
 //local
- $config->set('dbhost', 'localhost');
- $config->set('dbname', 'travel');
- $config->set('dbuser', 'root');
- $config->set('dbpass', '');
+// $config->set('dbhost', 'localhost');
+// $config->set('dbname', 'travel');
+// $config->set('dbuser', 'root');
+// $config->set('dbpass', '');
