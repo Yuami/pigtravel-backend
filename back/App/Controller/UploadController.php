0afff394a7fa68c509191bddf8b280c64b989195
JoanFornesRayo <j_fornes_1999@hotmail.com>
2019-03-14 10:23:22 +0100

    prueba

--- back/App/Controller/UploadController.php
+++ back/App/Controller/UploadController.php
@@ -61,6 +61,7 @@ class UploadController extends Controller
                             'idVivienda' => $idVivienda,
                             'idFoto' => $upload->getId(),
                         ]);
+                        dd("fin");
                     }
                 }
             }
